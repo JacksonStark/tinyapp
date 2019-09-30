0d1daf1cf90ce80bcc0d6c9a772bb9d77fa1c326
Jackson Stark <jacksonstark77@hotmail.com>
2019-09-30 20:30:53 +0000

    implemented bootstrap

--- express_server.js
+++ express_server.js
@@ -2,18 +2,29 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselsbd.ca',
   '9sm5zK': 'http://www.google.com'
 };
 
+// APP/SERVER LISTENING...
+
+app.listen(PORT, () => {
+  console.log(`Example app sslistxxening on port ${PORT}!`);
+});
+
+// GET REQUESTS
+
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars)
+})
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
