d3ef76a18805a9ba5efbd0fd5ab6a76a86977066
Jackson Stark <jacksonstark77@hotmail.com>
2019-09-30 23:45:47 +0000

    implemented get and post requests and responses

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -15,10 +17,32 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+const generateRandomString = () => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+// POST REQUESTS
+
+app.post('/urls', (req, res) => {
+  let newShortUrl = generateRandomString()
+  urlDatabase[newShortUrl] = req.body.longURL;
+  res.redirect(`/urls/${newShortUrl}`);
+  console.log(urlDatabase);
+})
+
 // GET REQUESTS
 
 app.get('/', (req, res) => {
   res.send('Hello');
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.get('/urls', (req, res) => {
@@ -26,8 +50,16 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars)
 })
 
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+})
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+})
+
 app.get('/urls/:shortURL', (req, res) => {
-  console.log('hello')
   const shortURL = req.params.shortURL;
   let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL]};
   res.render('urls_show', templateVars);
