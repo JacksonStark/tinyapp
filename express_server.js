70ab6b484e1c310868e8fa67df8bbe7f250fd2cd
Jackson Stark <jacksonstark77@hotmail.com>
2019-10-02 22:17:49 +0000

    lots of new features

--- express_server.js
+++ express_server.js
@@ -15,6 +15,9 @@ const urlDatabase = {
   '9sm5zK': 'http://www.google.com'
 };
 
+const users = {
+
+};
 
 // APP/SERVER LISTENING...
 
@@ -22,10 +25,9 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-
 // OTHER FUNCTIONS AND METHODS...
 
-const generateRandomString = () => {
+const generateRandomString = () => { // creates unique id
   let result = '';
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < 6; i++) {
@@ -34,6 +36,14 @@ const generateRandomString = () => {
   return result;
   }
 
+const emailLookup = (email) => { // checks if email is in database
+  for (const user in users) {
+    if (users[user].email === email) {
+      return users[user];
+    }
+  }
+  return false;
+}
 
 // POST REQUESTS
 
@@ -55,13 +65,45 @@ app.post('/urls/:shortURL/edit', (req, res) => { // modifies existing url and re
 })
 
 app.post('/login', (req, res) => { // logs in and records username (cookie)
-  res.cookie('username', req.body.username)
+  const user = emailLookup(req.body.email);
+
+  if (!user) { // Sending error if email is not in system
+    res.status(403);
+    let templateVars = { statusCode: res.statusCode, message: 'Email cannot be found'}
+    res.render('error', templateVars);
+  } else if (req.body.password !== user.password) { // Sending error if password is incorrect
+    res.status(403);
+    let templateVars = { statusCode: res.statusCode, message: 'Password is incorrect'} 
+    res.render('error', templateVars);
+  }
+
+  res.cookie('user', user);
   res.redirect('/urls');
 })
 
-app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+app.post('/logout', (req, res) => { // logs out and clears username (cookie)
+  res.clearCookie('user');
   res.redirect('/urls');
+})
+
+app.post('/register', (req, res) => { // creates new account and username cookie, records them in database
+  const email = req.body.email;
+  const password = req.body.password;
+  const uID = generateRandomString();
+  if (!email || !password) {
+    res.status(400);
+    let templateVars = { statusCode: res.statusCode, message: ' Password or Email not entered!'}
+    res.render('error', templateVars)
+  } else {
+    users[uID] = {};
+    const user = users[uID];
+    user.id = uID;
+    user.email = email;
+    user.password = password;
+    res.cookie('user', user);
+    console.log('Users Object: ', users);
+    res.redirect('/urls')
+  }
 })
 
 
@@ -76,22 +118,30 @@ app.get("/urls.json", (req, res) => { // get .json of urls
 });
 
 app.get('/urls', (req, res) => { // get urls index page
-  let templateVars = { urls: urlDatabase, username: req.cookies['username'] };
+  let templateVars = { urls: urlDatabase, user: req.cookies['user'] };
   res.render('urls_index', templateVars)
-})
+});
 
 app.get('/urls/new', (req, res) => { // get Create New URl page
-  res.render('urls_new');
-})
+  let templateVars = { user: req.cookies['user'] }
+  res.render('urls_new', templateVars);
+});
 
 app.get('/u/:shortURL', (req, res) => { // redirect to long url when short is clicked
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
-})
+});
 
 app.get('/urls/:shortURL', (req, res) => { // shows the urls_show page
   const shortURL = req.params.shortURL;
-  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL], username: req.cookies['username']};
+  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL], user: req.cookies['user']};
   res.render('urls_show', templateVars);
-})
+});
 
+app.get('/register', (req, res) => { // get Register page
+  res.render('register');
+});
+
+app.get('/login', (req, res) => { // get Login page
+  res.render('login');
+});
